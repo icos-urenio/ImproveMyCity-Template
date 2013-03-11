8f5be3a0ca795ce353082b8bb9f44b2f3d1f83a5
Ioannis Tsampoulatidis <tsampoulatidis@gmail.com>
2013-03-11 15:23:24 +0200

    remove popup

--- html/mod_login/default.php
+++ html/mod_login/default.php
@@ -5,12 +5,18 @@
 // no direct access
 defined('_JEXEC') or die;
 
+
 JHtml::_('behavior.keepalive');
 $document = JFactory::getDocument();
-$document->addScript(JURI::root(true).'/templates/improvemycity/assets/js/bootstrap.min.js');
 
-JHTML::_('behavior.modal', 'a.modalwin', array('handler' => 'ajax')); /* fix */
+//$document->addScript(JURI::root(true).'/templates/improvemycity/js/jquery-1.7.1.min.js');
+//$document->addScript(JURI::root(true).'/templates/improvemycity/assets/js/bootstrap.min.js');
+//$document->addStyleSheet(JURI::root(true).'/templates/improvemycity/assets/css/bootstrap.min.css');
+
+//JHTML::_('behavior.modal', 'a.modalwin', array('handler' => 'ajax', 'evalScripts' => true)); /* fix */
+
 ?>
+
 <div id="mod-login">
 <?php if ($type == 'logout') : ?>
 	
@@ -26,20 +32,22 @@ JHTML::_('behavior.modal', 'a.modalwin', array('handler' => 'ajax')); /* fix */
 		</div>
 		<?php endif; ?>
 
-		<div class="btn-group">
-			<button class="btn"><i class="icon-user"></i> <?php echo $greet;?></button>
-			<button class="btn dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></button>
+		
 			<form name="logoutform" action="<?php echo JRoute::_('index.php', true, $params->get('usesecure')); ?>" method="post">
-				<ul class="dropdown-menu">
-					<!-- dropdown menu links -->
-					<li><a href="#" onclick="javascript:document.logoutform.submit();;"><i class="icon-off"></i> <?php echo JText::_('JLOGOUT');?></a></li>
-				</ul>
-			<input type="hidden" name="option" value="com_users" />
-			<input type="hidden" name="task" value="user.logout" />
-			<input type="hidden" name="return" value="<?php echo $return; ?>" />
-			<?php echo JHtml::_('form.token'); ?>	
+				
+				<input type="hidden" name="option" value="com_users" />
+				<input type="hidden" name="task" value="user.logout" />
+				<input type="hidden" name="return" value="<?php echo $return; ?>" />
+				<?php echo JHtml::_('form.token'); ?>	
+				<div class="btn-group">
+				<?php /*
+				<a class="btn" href="index.php?option=com_users&view=profile&return=<?php echo $return; ?>"><i class="icon-user"></i> <?php echo $greet;?></a>
+				*/?>
+				<a class="btn" href="javascript:void(0);"><i class="icon-user"></i> <?php echo $greet;?></a>
+				<a class="btn" onclick="javascript:document.logoutform.submit();"><i class="icon-off"></i> <?php echo JText::_('JLOGOUT');?></a>
+				</div>
 			</form>
-		</div>
+		
 
 <?php else : ?>
 		
@@ -47,12 +55,11 @@ JHTML::_('behavior.modal', 'a.modalwin', array('handler' => 'ajax')); /* fix */
 		$usersConfig = JComponentHelper::getParams('com_users');
 		if ($usersConfig->get('allowUserRegistration')) : ?>
 			<div class="btn-group">
-				
-				<a class="btn modalwin" rel="{size: {x: 270, y: 360}}" href="index.php?option=com_users&view=login&tmpl=component&return=<?php echo $return; ?>"><?php echo JText::_('JLOGIN');?></a>
+				<a class="btn" href="index.php?option=com_users&view=login&return=<?php echo $return; ?>"><?php echo JText::_('JLOGIN');?></a>
 				<a class="btn" href="index.php?option=com_users&view=registration&return=<?php echo $return; ?>"><?php echo JText::_('MOD_LOGIN_REGISTER');?></a>
 			</div>		
 		<?php else : ?>
-			<a class="btn modal" rel="{size: {x: 270, y: 320}}" href="index.php?option=com_users&view=login&tmpl=component&return=<?php echo $return; ?>"><?php echo JText::_('JLOGIN');?></a>		
+			<a class="btn" href="index.php?option=com_users&view=login&return=<?php echo $return; ?>"><?php echo JText::_('JLOGIN');?></a>
 		<?php endif; ?>
 
 <?php endif; ?>
